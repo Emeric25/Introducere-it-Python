8c7075c52bd6b238a75aacdbf318f3cffc25242a
Emeric25 <148281928+Emeric25@users.noreply.github.com>
2025-12-04 20:32:50 +0200

    Add files via upload

--- Intro.py
+++ Intro.py
@@ -72,3 +72,52 @@
 # #impartire
 # print(5 / 2 )
 # print(5 // 2)
+
+# print('testing Python')
+# print('itschool rooookc')
+
+# text = 'Python'
+# numar_intreg = 20
+# numar_zecimal = 5.6
+
+# print(type(text))
+# print(type(numar_intreg))
+# print(type(numar_zecimal))
+
+# print(type(numar_intreg))
+# # numar_intreg = str(numar_intreg)
+# numar_intreg_convert_to_string = str(numar_intreg)
+# print(type(numar_intreg_convert_to_string))
+
+# varsta = 20
+# nume = 'Mihail'
+# print(nume + ' are ' + str(varsta) + ' ani ' )
+
+# text = "30"
+# print(int(text) + 10)
+
+#cerem utilizatorului varsta
+#calculam si anuntam cati ani mai are de trait pana la 100 de ani
+
+#Varianta mea
+# print("""Buna
+#       Cum te numesti?""")
+# nume = input("Nume :")
+# print("Cati ani ai?")
+# varsta = input ("Varsta :")
+
+# varsta = int(varsta)
+# # print(nume + " mai ai de trait")
+# rezultat= 100 - varsta 
+# rezultat_str = str(rezultat)
+# print(nume + ' mai ai ' + rezultat_str + ' ani de trait ')
+
+#Vrianta lui Bogdan
+
+# nume = input("Nume : ")
+# varsta_str = input('Varsta : ')
+# varsta_int = int(varsta_str)
+# rezultat = 100 - varsta_int
+# rezultat_str = str(rezultat)
+
+# print(' Salut ' + nume + ', mai ai de trait ' + rezultat_str + ' de ani pana la 100.')
