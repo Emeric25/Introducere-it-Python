79fcd045c9d0703be22f08c4c7956a0f2f10ef0b
Emeric25 <148281928+Emeric25@users.noreply.github.com>
2025-12-04 20:24:48 +0200

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,74 @@
+# nume = "Mihail"
+# nume = "Maria"
+# print(nume)
+
+#mai sus am printat ceva
+#Comentarile sunt bune pentru developeri pentru a documenta codul
+
+# text1 = 'Aici pot scrie un "text" simplu'
+# text2 ="Mihail's bike is blue"
+# text3 = """aici
+# putem
+# scrie
+# pe
+# mai
+# multe
+# linii"""
+# print(text1)
+# print(text2)
+# print(text3)
+
+# Sa scriem o poezie cu Titlu, Autor si o strofa
+
+# Eminescu = """Mihai Eminescu
+#         Adio
+#     De-acuma nu te-oi mai vedea,
+#     Rămâi, rămâi, cu bine!
+#     Mă voi feri în calea mea
+#     De tine.
+# """
+# print(Eminescu)
+
+# titlu = "               Adio        "
+# autor = "Mihai Eminescu"
+# strofa = """        De-acuma nu te-oi mai vedea,
+#         Rămâi, rămâi, cu bine!
+#         Mă voi feri în calea mea
+#         De tine.
+#            """
+# print(titlu)
+# print(autor)
+# print(strofa)
+
+# numar_intreg = 5 
+# print(numar_intreg)
+# numar_zecimal = 5.5
+# print(numar_zecimal)
+
+# print("Numele tau care este?")
+# nume = input("Scrieti aici numele> ")
+# print("Ma bucur sa te cunosc ")
+# print(nume)
+
+# text1 = "Mihai merge"
+# text2 = "la mare"
+# # print (text1 + " " + text2)
+# text3 = text1 + " " + text2
+# print(text3)
+
+# text = """It School
+# """
+# print(text*10)
+
+# #Adunarea
+# print(7+8)
+
+# #Scaderea
+# print(10 - 3)
+
+# #Inmultire
+# print(5 * 2)
+
+# #impartire
+# print(5 / 2 )
+# print(5 // 2)
