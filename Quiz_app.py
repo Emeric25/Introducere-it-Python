347b10d225acc587c2b4c78c83731b6b5b54d1f6
Emeric25 <148281928+Emeric25@users.noreply.github.com>
2025-12-30 17:05:43 +0200

    Add files via upload

--- Quiz_app.py
+++ Quiz_app.py
@@ -1,18 +1,39 @@
 import random
-utilizator = input("""Cum te numesti?
->""")
-print("Buna " + utilizator + ", te rugam sa raspunzi cu 't'(True) sau 'F'(False) la urmatoarele intrebari:")
-lista_intrebari=[
-    ["Este adevarat ca o propozitie este un str?"],
-    ["Este adevarat ca lista incepe de la 1?"],
-    ["Este adevarat ca Pythonul este usor?"],
-    ["Este adevarat ca vine anul nou?"],
-    ["Este adevarat ca in Franta este diferit fusul orar?"]
-]
-for elemente in lista_intrebari[0][0]:
-    print(elemente)
-    if elemente [0][0] == "t":
-        print("corect")
 
-random.shuffle(lista_intrebari)
-print(lista_intrebari[0])
+# Inceputul aplicatiei Quiz
+
+print("Quiz app")
+utilizator = input("""Cum te numesti?
+""")
+print("Bine ai venit " + utilizator + " instructiunile sunt urmatoarele:")
+print(utilizator + ", te rog sa raspunzi cu 't'(True) sau 'F'(False) la urmatoarele intrebari")
+print("Scorul final va fi afisat")
+
+# Lista de intrebari care se adauga una dupa cealalta
+
+lista_intrebari=[]
+lista_intrebari.append(["Este adevarat ca o propozitie este un str?","T"])
+lista_intrebari.append(["Este adevarat ca lista incepe de la 1?","F"])
+lista_intrebari.append(["Este adevarat ca Pythonul este usor?","F"])
+lista_intrebari.append(["Este adevarat ca vine anul nou?","T"])
+lista_intrebari.append(["Este adevarat ca in Franta este diferit fusul orar?","T"])
+
+scor = 0
+
+# Afiseaza intrebarile utilizatorului si ii cere un raspins
+
+for intrebare in lista_intrebari:
+    print()
+    print(intrebare[0])
+    print()
+    raspuns = input (":")
+    if raspuns.upper() [0] == intrebare[1]:
+        scor = scor + 1
+        print("corect")
+    else:
+        print("incorect")
+print (scor)
+
+# nu am stiut cum sa folosesc random va trebui sa reincerc
+# random.shuffle(lista_intrebari)
+# print(lista_intrebari[0])
